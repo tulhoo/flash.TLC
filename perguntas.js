d60a65fba191bba4dfecadc7b4a5c394d5397486
tulhoo <142426070+tulhoo@users.noreply.github.com>
2024-11-18 11:53:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,50 @@
+criaCartao(
+    'THE LOST CANVAS',
+    'Qual o nome do cavaleiro de Pegasus?',
+    'Tenma'
+)
+
+
+criaCartao(
+    'THE LOST CANVAS',
+    'Quem reencarnou Athena e Hades?',
+    'Sasha e Alone'
+)
+
+
+criaCartao(
+    'THE LOST CANVAS',
+    'Qual o nome dos dois irmãos Deuses da Morte e do Sono? E quem foram os que mataram Eles?',
+    'Hypnos e Tanatos, os dois foram mortos por por Sage e Hakurei respectivamente'
+)
+
+
+criaCartao(
+    'THE LOST CANVAS',
+    'Como tenma consegue o 6° Sentido?',
+    'Com seu treinamento na ilha Kanon'
+)
+
+criaCartao(
+    'THE LOST CANVAS',
+    'Como El Cid de capricornio morreu?',
+    'Na luta contra os 4 deuses do mundo dos sonhos, Fantasis, Morfeu Oneiros e Ìcelos.'
+)
+
+
+criaCartao(
+    'THE LOST CANVAS',
+    'Para onde Degel de Aquario vai depois que o anime acaba se a trama recomeça no mangá?',
+    'Para Blue Graad, junto com Kardia'
+)
+criaCartao(
+    'THE LOST CANVAS',
+    'Quem é o cavaleiro de ouro mais forte?',
+    'Não se sabe ao certo, mas El Cid teve os melhores feitos entre todos'
+) 
+
+criaCartao(
+    'THE LOST CANVAS',
+    'Oque protegue Tenma das forças de Hades?',
+    'A pulceira de flores da Sasha.'
+)
